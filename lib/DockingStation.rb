f275195826c9abc3f934b24207c58cd2a2030178
twigz826 <thomastwigden@gmail.com>
2021-01-05 14:56:21 -0300

    Added test to boris_bikes_spec.rb file and created method in DockingStation.rb

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,3 +1,5 @@
 class DockingStation
-
+  def self.release_bike
+    p "IN THE METHOD"
+  end
 end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,5 +1,9 @@
 require 'DockingStation'
 
 describe DockingStation do
-
+  #describe "responds to the method release_bike" do
+    #it { is_expected.to respond_to release_bike }
+  it "responds to the method release_bike" do
+    expect(DockingStation).to respond_to :release_bike
+  end
 end
